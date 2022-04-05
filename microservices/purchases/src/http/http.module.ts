40a9a591e5e394c8c15955d466447ab72fdcdecb
x0n4d0 <romagnolimauricio@gmail.com>
2022-04-05 18:30:58 -0300

    build: add @nestjs/config

--- microservices/purchases/src/http/http.module.ts
+++ microservices/purchases/src/http/http.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class HttpModule {}
